09bffff093698e0f9236bf557ed0408973c2a2c5
Alberttt <q09626@aliyun.com>
2021-08-30 23:47:02 +0800

    O4-2

--- src/O4二维数组中的查找.go
+++ src/O4二维数组中的查找.go
@@ -1,1 +1,18 @@
 package main
+
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	if matrix == nil || len(matrix) == 0 || matrix[0] == nil || len(matrix[0]) == 0 {
+		return false
+	}
+	i, j := 0, len(matrix[0])-1
+	for i < len(matrix) && j >= 0 {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return false
+}
