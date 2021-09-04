ac4de60236c2c9ebccfab42630951d63475e33f6
Alberttt <q09626@aliyun.com>
2021-09-04 13:06:16 +0800

    01背包，双重循环里面的逻辑更新

--- src/U背包问题(01).go
+++ src/U背包问题(01).go
@@ -3,9 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	n, c := 5, 10
-	v := []int{0, 8, 10, 4, 5, 5} // 补0，下标i=第i个
-	w := []int{0, 6, 4, 2, 4, 3}
+	//n, c := 5, 10
+	//v := []int{0, 8, 10, 4, 5, 5} // 补0，下标i=第i个
+	//w := []int{0, 6, 4, 2, 4, 3}
+
+	var n, c int
+	fmt.Scan(&n, &c)
+
+	var v []int = make([]int, n+1)
+	var w []int = make([]int, n+1)
+	v[0] = 0
+	w[0] = 0
+
+	for i := 1; i <= n; i++ {
+		var vi, wi int
+		fmt.Scan(&vi, &wi)
+		v[i] = vi
+		w[i] = wi
+	}
 
 	dp := make([][]int, n+1) // 长度加1
 	for i := 0; i < n+1; i++ {
@@ -23,13 +38,11 @@ func main() {
 	// 填表
 	for i := 1; i < n+1; i++ {
 		for j := 1; j < c+1; j++ {
+			dp[i][j] = dp[i-1][j]
 			if w[i] > j {
-				dp[i][j] = dp[i-1][j]
 				continue
 			}
-			if dp[i-1][j] > dp[i-1][j-w[i]]+v[i] {
-				dp[i][j] = dp[i-1][j]
-			} else {
+			if dp[i-1][j] < dp[i-1][j-w[i]]+v[i] {
 				dp[i][j] = dp[i-1][j-w[i]] + v[i]
 			}
 		}
